a94e9694268ba028de96766369eabd16a80e7241
”wangyuchenAbel“ <wangyuchenabel@gmail.com>
2024-10-28 15:56:22 +0800

    some code

--- components/header.vue
+++ components/header.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="header-contianer">
-    <div class="logo"></div>
-    <div class="menu"></div>
-    <div class="media"></div>
+    <div class="logo">logo</div>
+    <div class="menu">
+      <div>Index</div>
+      <div>Article</div>
+      <div>Project</div>
+      <div>Inspiration</div>
+    </div>
+    <div class="media">github</div>
   </div>
 </template>
+<style scoped>
+.header-contianer {
+  display: flex;
+  padding: 20px 20px 0;
+  flex-wrap: wrap;
+  .menu {
+    display: flex;
+  }
+}
+</style>
